604dfc17e0cad49e9bc81e01324a47757ccc89eb
Ratan khandekar <ratankhandear2@gmail.com>
2023-03-21 13:37:10 +0530

    deletefunctional

--- src/main/java/com/second/MyController.java
+++ src/main/java/com/second/MyController.java
@@ -3,32 +3,37 @@ package com.second;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
-	
+
 	@Autowired
 	StudentRepository studentRepository;
-	
+
 	@RequestMapping("save")
-	int save(@RequestBody Student student)
-	{
-		
+	int save(@RequestBody Student student) {
+
 		studentRepository.save(student);
 		return 1;
 	}
-	
+
 	@RequestMapping("getAll")
-	List<Student> getAll()
-	{
-		List<Student> list= studentRepository.findAll();
+	List<Student> getAll() {
+		List<Student> list = studentRepository.findAll();
 		return list;
 	}
 	
+	@RequestMapping("delete/{id}")
+	String delete(@PathVariable int id)
+	{
+		studentRepository.deleteById(id);
+		return "delete";
+	}
 	
 	
-	
+
 }
